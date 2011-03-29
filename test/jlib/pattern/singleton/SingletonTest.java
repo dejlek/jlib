b12b6f6a3f24244b3cd7a646cfcece68836d23bf
Dejan Lekic <dejan@lekic.org>
2011-03-29 14:03:23 +0000

    InheritedSingletonTest removed from the test...
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@5 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- test/jlib/pattern/singleton/SingletonTest.java
+++ test/jlib/pattern/singleton/SingletonTest.java
@@ -21,15 +21,45 @@ public class SingletonTest {
     public SingletonTest() {
     }
 
-	/**
-	 * Test of init method, of class Singleton.
-	 */ @Test
-	public void testInit() {
-		System.out.println("init+getInstance");
-		String[] params = null;
-		InheritedSingletonTest.init(params);
-		if (InheritedSingletonTest.getInstance().getVal() != 5)
-			fail("The test case is a prototype.");
-	}
+   @BeforeClass
+   public static void setUpClass() throws Exception {
+   }
+
+   @AfterClass
+   public static void tearDownClass() throws Exception {
+   }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+   /**
+    * Test of init method, of class Singleton.
+    */
+   @Test
+   public void testInit() {
+      System.out.println("init");
+      String[] params = null;
+      Singleton.init(params);
+      // TODO review the generated test code and remove the default call to fail.
+      fail("The test case is a prototype.");
+   }
+
+   /**
+    * Test of getInstance method, of class Singleton.
+    */
+   @Test
+   public void testGetInstance() {
+      System.out.println("getInstance");
+      Singleton expResult = null;
+      Singleton result = Singleton.getInstance();
+      assertEquals(expResult, result);
+      // TODO review the generated test code and remove the default call to fail.
+      fail("The test case is a prototype.");
+   }
 
 }
