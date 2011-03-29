9f20d8b3273b3f927d6c89e44ae4a8128a78e6f9
Dejan Lekic <dejan@lekic.org>
2011-03-29 14:03:21 +0000

    InheritedSingletonTest removed from the test...
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@4 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- src/jlib/pattern/singleton/Singleton.java
+++ src/jlib/pattern/singleton/Singleton.java
@@ -1,8 +1,41 @@
+
 package jlib.pattern.singleton;
 
 /**
- * Singleton pattern -  the most simple implementation.
- *
+ * Singleton pattern - the simplest implementation.
+ * 
+ * EXAMPLE of usage:
+ ******************* 
+public class InheritedSingletonTest extends Singleton {
+
+	private final static int DEFAULT_VALUE = 5;
+	private final int value;
+
+	public static void init(final String[] params) {
+		instance = new InheritedSingletonTest(params, DEFAULT_VALUE);
+	}
+
+	public static void init(final String[] params, final int value) {
+		instance = new InheritedSingletonTest(params, value);
+	}
+
+	public static InheritedSingletonTest getInstance() {
+		return (InheritedSingletonTest)instance;
+	}
+
+	public int getVal() {
+		return value;
+	}
+
+	protected InheritedSingletonTest(final String[] params, final int value) {
+		super(params);
+		this.value = value;
+	}
+} 
+ *******************
+ * As shown above, Singleton should be inherited by the application's singleton,
+ * and application-specific functionality added as additional methods.
+ * 
  * Code from the "Hardcode Java", p62.
  */
 public class Singleton {
@@ -19,5 +52,6 @@ public class Singleton {
 
 	public static Singleton getInstance() {
 		return Singleton.instance;
-	}
+	} // getInstance() method
+   
 } // Singleton class
