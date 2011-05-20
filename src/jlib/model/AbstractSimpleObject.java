02c3ac162cbc029fcc4e69ca769f44adcedc154c
Dejan Lekic <dejan@lekic.org>
2011-05-20 14:06:38 +0000

    Added the AbstractSimpleObject class which has getNumberOfFields as a STATIC method. Every SimpleObject implementation should inherit this class.
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@20 bee1aa47-a06f-436c-a2f2-6a1efea3c100

---
+++ src/jlib/model/AbstractSimpleObject.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jlib.model;
+
+/**
+ * The reason we have this class is the fact that (stupid) Java can't have static method declarations in
+ * an interface. (static modifier not allowed in method declarations of an interface)
+ * 
+ * @author dejan
+ */
+public abstract class AbstractSimpleObject {
+    
+    /// Returns a number of fields inside underlying ValueObject
+    public static byte getNumberOfFields() { return 0; }
+    
+} // AbstractSimpleObject class
+
+// $Id$
