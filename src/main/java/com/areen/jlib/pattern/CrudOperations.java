aeda627d8c6f45df10255cc522469bb6a95f717f
Dejan Lekic <dejan@lekic.org>
2011-07-17 09:43:06 +0000

    set() and get() methods put into the CrudOperations interface.
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@67 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- src/main/java/com/areen/jlib/pattern/CrudOperations.java
+++ src/main/java/com/areen/jlib/pattern/CrudOperations.java
@@ -1,16 +1,42 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * $Id$
+ *
+ * Copyright (c) 2009-2010 Areen Design Services Ltd
+ * 23 Eyot Gardens; London; W6 9TR
+ * http://www.areen.com
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of
+ * Areen Design Services Ltd ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Areen Design Services Ltd.
+ * 
+ * This file is best viewed with 110 columns.
+12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+ * 
+ * This is the main class for the "apc-client" application. 
+ * Execute apc-client like this: java -jar apc-client.jar -Dareen.developer=dejan
+ * You can ommit the parameter if you do not want to debug the application.
+ * 
+ * Author(s) in chronological order:
+ *   Dejan Lekic , http://dejan.lekic.org
+ * Contributor(s):
+ *   -
  */
+
 package com.areen.jlib.pattern;
 
 /**
- *
+ * This interface formulates what methods must be implemented by any CRUD operations implementation.
+ * 
  * @author dejan
  */
 public interface CrudOperations<T extends ValueObject> {
-    public void setOldValue(Object argOldValue);
-    public void setArgs(String argInput); /// Use this method to specify parameters to the CRUD object.
+    public void    setOldValue(Object argOldValue);
+    public void    setArgs(String argInput); /// Use this method to specify parameters to the CRUD object.
+    public void    set(String argName, Object argValue);
+    public Object  get(String argName);
     // CREATE -------------------------------------------
     public T       create();
     public T       create(T argVo);
