2412c543419a4d373d2d715c39e7c4f9e1947e0a
Dejan Lekic <dejan@lekic.org>
2011-08-08 15:08:05 +0000

    SmallArrayIntTest class for testing the SmallArray classes.
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@76 bee1aa47-a06f-436c-a2f2-6a1efea3c100

---
+++ src/test/java/com/areen/jlib/ds/SmallArrayIntTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.areen.jlib.ds;
+
+import com.areen.jlib.exceptions.SmallArrayException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dejan
+ */
+public class SmallArrayIntTest {
+    SmallArrayInt testArray;
+    
+    public SmallArrayIntTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testConstructor() {
+        System.out.println("TEST: testConstructor()");
+        try {
+            // TODO review the generated test code and remove the default call to fail.
+            testArray = new SmallArrayInt(3);
+        } catch (SmallArrayException ex) {
+            Logger.getLogger(SmallArrayIntTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        assertTrue("Incorrect number of elements. Got " + testArray.size() + ", expected 10.", (testArray.size() == 10));
+    } // testConstructor() method
+    
+} // SmallArrayIntTest class
+
+// $Id$
