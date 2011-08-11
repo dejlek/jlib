fd11a5886c29ce2158d885a9b8155042e361d301
Dejan Lekic <dejan@lekic.org>
2011-08-11 10:21:18 +0000

    Properly formatted code.
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@78 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- src/main/java/com/areen/jlib/util/Utility.java
+++ src/main/java/com/areen/jlib/util/Utility.java
@@ -36,74 +36,75 @@ import java.util.Vector;
  */
 public class Utility {
 
-	public static String getLineInfo() {
+    public static String getLineInfo() {
         StackTraceElement ste = new Throwable().getStackTrace()[1];
         return ste.getFileName() + ": Line " + ste.getLineNumber();
     } // getLineInfo() method
-	
-	/**
-	 * Function to export csv file
-	 * @param file
-	 * @param h1
-	 * @param h2
-	 * @param headings
-	 * @param data
-	 */
-	public static void exportToCSV(String file, String h1, String h2, Vector<String> headings, Vector<Vector<String>> data){
-		try {
-			FileOutputStream out = new FileOutputStream(file);
-			PrintStream ps = new PrintStream(out);
-			
-			ps.println(h1);
-			ps.println(h2);
-			ps.println();
-			
-			//print headings
-			Iterator<String> hi = headings.iterator();
-			
-			int i = 0;
-			while(hi.hasNext()){
-				ps.print(hi.next());
-				
-				if(i<headings.size()-1)
-					ps.print(",");
-				else 
-					ps.println();
-				i++;
-			}
-			
-			//print the data
-			Iterator<Vector<String>> di = data.iterator();
-			Iterator<String> si = null;
-			
-			while(di.hasNext()){
-				//get iterator
-				si = di.next().iterator();
-				i=0;
-				while(si.hasNext()){
-					ps.print(si.next());
-					
-					if(i<headings.size()-1)
-						ps.print(",");
-					else 
-						ps.println();
-					
-					i++;
-				}
-			}
-			
-			ps.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	} // exportToCSV() method
-        
-        public static void set(SimpleObject argSo, String argValue, int argIndex) {
-            if (argSo.getFieldClass(argIndex) == Double.class) {
-                argSo.set(argIndex, Double.parseDouble(argValue));
-            } // if
-        } // set() method
-	
+
+    /**
+     * Function to export csv file
+     * @param file
+     * @param h1
+     * @param h2
+     * @param headings
+     * @param data
+     */
+    public static void exportToCSV(String file, String h1, String h2, Vector<String> headings, Vector<Vector<String>> data) {
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            PrintStream ps = new PrintStream(out);
+
+            ps.println(h1);
+            ps.println(h2);
+            ps.println();
+
+            //print headings
+            Iterator<String> hi = headings.iterator();
+
+            int i = 0;
+            while (hi.hasNext()) {
+                ps.print(hi.next());
+
+                if (i < headings.size() - 1) {
+                    ps.print(",");
+                } else {
+                    ps.println();
+                }
+                i++;
+            }
+
+            //print the data
+            Iterator<Vector<String>> di = data.iterator();
+            Iterator<String> si = null;
+
+            while (di.hasNext()) {
+                //get iterator
+                si = di.next().iterator();
+                i = 0;
+                while (si.hasNext()) {
+                    ps.print(si.next());
+
+                    if (i < headings.size() - 1) {
+                        ps.print(",");
+                    } else {
+                        ps.println();
+                    }
+
+                    i++;
+                }
+            }
+
+            ps.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } // exportToCSV() method
+
+    public static void set(SimpleObject argSo, String argValue, int argIndex) {
+        if (argSo.getFieldClass(argIndex) == Double.class) {
+            argSo.set(argIndex, Double.parseDouble(argValue));
+        } // if
+    } // set() method
 } // Utility class
 
 // $Id$
