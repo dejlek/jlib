86349cc70169643ba190f8f1543ae5efd1dad3ca
Dejan Lekic <dejan@lekic.org>
2011-10-12 14:09:08 +0000

    The initial version of the MenuButton class used in AreenTableToolBar Related issue(s): APC-482
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@84 bee1aa47-a06f-436c-a2f2-6a1efea3c100

---
+++ src/main/java/com/areen/jlib/gui/MenuButton.java
@@ -0,0 +1,47 @@
+/**
+ * $Id: AreenTablePanel.java 157 2011-10-12 12:47:16Z vipul $
+ *
+ * Copyright (c) 2009-2010 Areen Design Services Ltd
+ * 23 Eyot Gardens; London; W6 9TR
+ * http://www.areen.com
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of
+ * Areen Design Services Ltd ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Areen Design Services Ltd.
+ * 
+ * This file is best viewed with 110 columns.
+12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+ * 
+ * Author(s) in chronological order:
+ *   Dejan Lekic , http://dejan.lekic.org
+ * Contributor(s):
+ *   -
+ */
+
+package com.areen.jlib.gui;
+
+import javax.swing.Action;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author dejan
+ */
+public class MenuButton extends JButton {
+    public MenuButton() {
+        super();
+    }
+    public MenuButton(javax.swing.Action argAction) {
+        super(argAction);
+    }
+    public MenuButton(String argLabel) {
+        super(argLabel);
+    }
+
+    
+}
+
+// $Id$
