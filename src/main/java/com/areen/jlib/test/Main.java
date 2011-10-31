3a03a28c6a809025e875e419ebbeca8d94ff40c1
Dejan Lekic <dejan@lekic.org>
2011-10-31 10:16:10 +0000

    Main.java added - for future testing...
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@98 bee1aa47-a06f-436c-a2f2-6a1efea3c100

---
+++ src/main/java/com/areen/jlib/test/Main.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.areen.jlib.test;
+
+/**
+ *
+ * @author dejan
+ */
+public class Main {
+    
+}
