98a3cb46b5e02f14b59464598a473eb91cf62ae5
Dejan Lekic <dejan@lekic.org>
2011-12-21 11:13:25 +0000

    FilteredComboBoxTestFrame - initial implementation. Related issue(s): APC-613
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@141 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- src/main/java/com/areen/jlib/test/fcb/FilteredComboBoxTestFrame.java
+++ src/main/java/com/areen/jlib/test/fcb/FilteredComboBoxTestFrame.java
@@ -4,17 +4,22 @@
  */
 package com.areen.jlib.test.fcb;
 
+import com.areen.jlib.gui.FilteredComboBox;
+import javax.swing.DefaultCellEditor;
+
 /**
  *
  * @author dejan
  */
 public class FilteredComboBoxTestFrame extends javax.swing.JFrame {
-
+    FcbCellEditor fce;
+    
     /**
      * Creates new form FilteredComboBoxTestFrame
      */
     public FilteredComboBoxTestFrame() {
         initComponents();
+        fce = new FcbCellEditor();
     }
 
     /**
@@ -24,19 +29,39 @@ public class FilteredComboBoxTestFrame extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jComboBox1 = new FilteredComboBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new java.awt.GridBagLayout());
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jButton1.setText("jButton1");
+        getContentPane().add(jButton1, new java.awt.GridBagConstraints());
+
+        jButton2.setText("jButton2");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+        getContentPane().add(jButton2, gridBagConstraints);
+
+        jButton3.setText("jButton3");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 0;
+        getContentPane().add(jButton3, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridwidth = 3;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_END;
+        getContentPane().add(jComboBox1, gridBagConstraints);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -44,7 +69,7 @@ public class FilteredComboBoxTestFrame extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         /*
          * Set the Nimbus look and feel
          */
@@ -82,5 +107,19 @@ public class FilteredComboBoxTestFrame extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox jComboBox1;
     // End of variables declaration//GEN-END:variables
+
+    public class FcbCellEditor extends DefaultCellEditor {
+        public FcbCellEditor() {
+            super(new FilteredComboBox());
+            getComponent().setName("Table.editor");
+            setClickCountToStart(2);
+            FilteredComboBox fcb = (FilteredComboBox) editorComponent;
+            fcb.putClientProperty("JComboBox.isTableCellEditor", Boolean.TRUE);
+        } // FcbCellEditor constructor (default)
+    } // FcbCellEditor class (nested)
 }
