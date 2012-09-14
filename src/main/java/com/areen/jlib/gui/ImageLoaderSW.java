3b9c51e793328537e4fb90a65f766b3f88703c24
Dejan Lekic <dejan@lekic.org>
2012-09-14 14:01:43 +0000

    ImageLoaderSW, PictureDialog, and testing application ImageLoaderFrame. These initial versions already work (more/less). Related issue(s): APC-1012
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@461 bee1aa47-a06f-436c-a2f2-6a1efea3c100

---
+++ src/main/java/com/areen/jlib/gui/ImageLoaderSW.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.areen.jlib.gui;
+
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.SwingWorker;
+
+/**
+ *
+ * @author Dejan
+ */
+public class ImageLoaderSW extends SwingWorker<Image, Void> {
+    JLabel imageLabel;
+    URL webAddress;
+    
+    public ImageLoaderSW(JLabel argLabel, URL argURL) {
+        imageLabel = argLabel;
+        webAddress = argURL;
+    }
+    
+    @Override
+    protected Image doInBackground() throws Exception {
+        Image image = null;
+        try {
+            image = ImageIO.read(webAddress);
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+        return image;
+    } // diInBackground() method
+
+    @Override
+    protected void done() {
+        try {
+            imageLabel.setIcon(new ImageIcon(get()));
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+            //Logger.getLogger(ImageLoaderSW.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ExecutionException ex) {
+            System.out.println(ex);
+            //Logger.getLogger(ImageLoaderSW.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } // 
+    
+} // ImageLoaderSW class
+
+// $Id$
