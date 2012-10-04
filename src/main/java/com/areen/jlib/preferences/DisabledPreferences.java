5be8fc2214521251ebff08a979b7823ce3fed3db
Dejan Lekic <dejan@lekic.org>
2012-10-04 15:58:39 +0000

    These two classes gives us means to disable preferences. Related issue(s): APC-1012
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@483 bee1aa47-a06f-436c-a2f2-6a1efea3c100

---
+++ src/main/java/com/areen/jlib/preferences/DisabledPreferences.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.areen.jlib.preferences;
+
+import java.util.prefs.AbstractPreferences;
+import java.util.prefs.BackingStoreException;
+
+/**
+ * Copyright 2003-2011 Robert Slifka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * A do-nothing Preferences implementation so that we can avoid the hassles
+ * of the JVM Preference implementations.
+ *
+ * @author Robert Slifka (robert.slifka@gmail.com)
+ */
+public class DisabledPreferences extends AbstractPreferences {
+
+    public DisabledPreferences() {
+        super(null, "");
+    }
+
+    @Override
+    protected void putSpi(String key, String value) {
+
+    }
+
+    @Override
+    protected String getSpi(String key) {
+        return null;
+    }
+
+    @Override
+    protected void removeSpi(String key) {
+
+    }
+
+    @Override
+    protected void removeNodeSpi() throws BackingStoreException {
+
+    }
+
+    @Override
+    protected String[] keysSpi() throws BackingStoreException {
+        return new String[0];
+    }
+
+    @Override
+    protected String[] childrenNamesSpi()
+            throws BackingStoreException {
+        return new String[0];
+    }
+
+    @Override
+    protected AbstractPreferences childSpi(String name) {
+        return null;
+    }
+
+    @Override
+    protected void syncSpi() throws BackingStoreException {
+
+    }
+
+    @Override
+    protected void flushSpi() throws BackingStoreException {
+
+    }
+}
+
+// $Id$ 
