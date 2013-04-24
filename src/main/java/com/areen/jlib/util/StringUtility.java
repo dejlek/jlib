870722240cc55fb8b312d0a5194a8c65b5187cba
Dejan Lekic <dejan@lekic.org>
2013-04-24 15:32:35 +0000

    Added the StringUtility class. Related issue(s): APC-1137
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@608 bee1aa47-a06f-436c-a2f2-6a1efea3c100

---
+++ src/main/java/com/areen/jlib/util/StringUtility.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.areen.jlib.util;
+
+/**
+ * Various (static) methods for all kind of String manipulation.
+ * 
+ * @author Dejan
+ */
+public class StringUtility {
+    
+    /**
+     * Get a "proper" String object that contains a commonly accepted format for user e-mail in the form
+     * "Fname Sname <email@example.com>" .
+     * 
+     * @param argFullName
+     * @param argEmail
+     * @return 
+     */
+    public static String toEmailString(String argFullName, String argEmail) {
+        return argFullName + "<" + argEmail + ">";
+    } // toEmailString() method
+    
+    /**
+     * Same as the method above, however here we expect argData to be in the form:
+     * "Fname Sname, email@example.com"
+     * 
+     * @param argData
+     * @return 
+     */
+    public static String toEmailString(String argData) {
+        String[] parts = argData.split(",");
+        return toEmailString(parts[0].trim(), parts[1].trim());
+    } // toEmailString() method
+    
+} // StringUtility class
+
+// $Id$
