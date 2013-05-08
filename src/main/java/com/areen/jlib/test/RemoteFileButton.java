fd5896e49ce82cd77423ab3d1496b71b8dfdc7fd
Dejan Lekic <dejan@lekic.org>
2013-05-08 14:17:01 +0000

    More simple changes.. Related issue(s): APC-1159
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@622 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- src/main/java/com/areen/jlib/test/RemoteFileButton.java
+++ src/main/java/com/areen/jlib/test/RemoteFileButton.java
@@ -22,22 +22,29 @@ import javax.swing.SwingUtilities;
 import javax.swing.border.BevelBorder;
 
 /**
- *
+ * JComponent made for the sole purpose of maintaining various remote files.
+ * 
+ * What it does:
+ *   1) It indicates clearly whether remote document is uploaded or not.
+ *   2) It is capable of triggering upload of file either by drag&drop, or by clicking on the button and
+ *      invoking the file select dialog so user can pick file to upload.
+ *   3) It has a button to delete the remote file in the case wrong file has been uploaded.
  * @author Dejan
  */
 public class RemoteFileButton extends JComponent {
+    private RemoteFileButtonModel model;
     
     public RemoteFileButton() {
         super();
+        model = new RemoteFileButtonModel();
         setBorder(new BevelBorder(BevelBorder.RAISED));
         setLayout(new BorderLayout());
         add(new JButton("[#]"), BorderLayout.WEST);
         add(new JLabel("Blah"), BorderLayout.CENTER);
         add(new JButton("[x]"), BorderLayout.EAST);
-    }
+    } // RemoteFileButton constructor (default)
             
     public static void main(String[] args) {
-        
         final JFrame frame = new JFrame();
         frame.setLayout(new BorderLayout());
         frame.add(new RemoteFileButton(), BorderLayout.NORTH);
@@ -50,6 +57,8 @@ public class RemoteFileButton extends JComponent {
             
         });
 
-    }
+    } // main() method
     
-}
+} // RemoteFileButton class
+
+// $Id$
