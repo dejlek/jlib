9998f2b94b1e9113225e3fadd5761fd023966052
Dejan Lekic <dejan@lekic.org>
2013-10-18 16:10:02 +0000

    Changed header. Related issue(s): APC-1788
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@728 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- src/main/java/com/areen/jlib/util/StackTraceUtil.java
+++ src/main/java/com/areen/jlib/util/StackTraceUtil.java
@@ -1,15 +1,36 @@
 /**
- * Project: jlib 
+ * Project: jlib
  * Version: $Id$ 
- * License: SPL
- *
- * This file is best viewed with 110 columns.
- * 34567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+ * License: (see below block, or the LICENSE file)
+ **************************************************************************************** LICENSE BEGIN *****
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including 
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, distribute with 
+ * modifications, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+ * is furnished to do so, subject to the following conditions:
  * 
- * Authors (in chronological order): 
- *   -
- * Contributors (in chronological order): 
+ * 1) The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO 
+ * EVENT SHALL THE ABOVE COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN 
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * Except as contained in this notice, the name(s) of the above copyright holders shall not be used in 
+ * advertising or otherwise to promote the sale, use or other dealings in this Software without prior written 
+ * authorisation.
+ ****************************************************************************************** LICENSE END *****
+ * 
+ * This file is best viewed with 110 columns.
+12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+ * 
+ * In chronological order
+ * Author(s):
  *   Dejan Lekic , http://dejan.lekic.org
+ * Contributor(s):
+ *   -
  */
 package com.areen.jlib.util;
 
