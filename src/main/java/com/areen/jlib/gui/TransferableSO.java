900f80cd9516f75dd492fda5daddb8e9c8ef026e
Dejan Lekic <dejan@lekic.org>
2013-10-30 10:57:19 +0000

    Cleanup of the JLIB. Related issue(s): APC-1788
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@735 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- src/main/java/com/areen/jlib/gui/TransferableSO.java
+++ src/main/java/com/areen/jlib/gui/TransferableSO.java
@@ -1,9 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Project: JLIB
+ * Version: $Id$ 
+ * License: (see below block, or the LICENSE file)
+ **************************************************************************************** LICENSE BEGIN *****
+ * Copyright (c) 2009-2013, JLIB AUTHORS (see the AUTHORS.txt file for the list of the individuals)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 
+ *  1) Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+ *     following disclaimer.
+ *  2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+ *     the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *  3) Neither the names of the organisations involved in the JLIB project, nor the names of their 
+ *     contributors to the JLIB project may be used to endorse or promote products derived from this software 
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************************** LICENSE END *****
+ * 
+ * This file is best viewed with 110 columns.
+12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+ * 
+ * In chronological order
+ * Author(s):
+ *   Dejan Lekic , http://dejan.lekic.org
+ * Contributor(s):
+ *   -
  */
-
 package com.areen.jlib.gui;
 
 import com.areen.jlib.model.SimpleObject;
@@ -12,57 +44,59 @@ import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
  * @author Dejan
  */
 public class TransferableSO<T extends SimpleObject> implements Transferable {
-    ArrayList<T> simpleObjects;
+    T[] simpleObjects; /// NOTE: It might be null!
+    
+    List<DataFlavor> flavorList;
+    DataFlavor[] flavors;
     
     private static final Class<?> RC1 = SimpleObject[].class; // Representation class
     private static final String RC1N = SimpleObject[].class.getCanonicalName(); // Human readable name
     private static final DataFlavor SIMPLE_OBJECT_ARRAY_DATA_FLAVOR = new DataFlavor(RC1, RC1N);
     
     public TransferableSO(ArrayList<T> argData) {
-        simpleObjects = argData;
-    }
+        flavorList = new ArrayList<DataFlavor>();
+        if (!argData.isEmpty()) {
+            T so = argData.get(0);
+
+            simpleObjects = argData.toArray((T[]) so.create(argData.size()));
+        } // if
+        
+        // Let's make list of supported flavors
+        flavors = new DataFlavor[1];
+        flavors[0] = SIMPLE_OBJECT_ARRAY_DATA_FLAVOR;
+        
+        flavorList = Arrays.asList(flavors);
+    } // TransferableSO constructor
 
     @Override
     public DataFlavor[] getTransferDataFlavors() {
-        DataFlavor[] ret = new DataFlavor[1];
-        ret[0] = SIMPLE_OBJECT_ARRAY_DATA_FLAVOR;
-        
-        return ret;
+        return flavors;
     }
 
     @Override
     public boolean isDataFlavorSupported(DataFlavor flavor) {
-        boolean ret = false;
-        
-        if ((flavor.getRepresentationClass() == RC1) && (flavor.getHumanPresentableName().equals(RC1N))) {
-            ret = true;
-        }
-                
-        return ret; //flavor.equals(getTransferDataFlavors()[0]);
+        return flavorList.contains(flavor);
     }
 
     @Override
-    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+    public synchronized Object getTransferData(DataFlavor flavor) 
+            throws UnsupportedFlavorException, IOException {
         Object ret = null;
         if (isDataFlavorSupported(flavor)) {
             ret = simpleObjects;
-        }
+        } // getTransferData() method
+        
         return ret;
     }
     
-    /*
-    If we want to implement move , we need this:
-    void exportDone(JComponent c, Transferable t, int action) {
-        if (action == MOVE) {
-            c.removeSelection();
-        }
-    }
-    */
+} // TransferableSO class
 
-}
+// $Id$
--- src/main/java/com/areen/jlib/gui/VoListTransferHandler.java
+++ src/main/java/com/areen/jlib/gui/VoListTransferHandler.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Project: JLIB
+ * Version: $Id$ 
+ * License: (see below block, or the accompanied COPYING.txt file)
+ **************************************************************************************** LICENSE BEGIN *****
+ * Copyright (c) 2009-2013, JLIB AUTHORS (see the AUTHORS.txt file for the list of the individuals)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 
+ *  1) Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+ *     following disclaimer.
+ *  2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+ *     the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *  3) Neither the names of the organisations involved in the JLIB project, nor the names of their 
+ *     contributors to the JLIB project may be used to endorse or promote products derived from this software 
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************************** LICENSE END *****
+ * 
+ * This file is best viewed with 110 columns.
+12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+ * 
+ * In chronological order
+ * Author(s):
+ *   Dejan Lekic , http://dejan.lekic.org
+ * Contributor(s):
+ *   -
  */
-
 package com.areen.jlib.gui;
 
 import com.areen.jlib.model.SimpleObject;
 import java.awt.datatransfer.Transferable;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 import javax.swing.JComponent;
 import javax.swing.TransferHandler;
 
@@ -17,6 +50,7 @@ import javax.swing.TransferHandler;
  * @author Dejan
  */
 public class VoListTransferHandler<T extends SimpleObject> extends TransferHandler {
+    public static final Logger LOGGER = Logger.getLogger(VoListTransferHandler.class.getCanonicalName());
 
     @Override
     public int getSourceActions(JComponent c) {
@@ -27,9 +61,15 @@ public class VoListTransferHandler<T extends SimpleObject> extends TransferHandl
     public Transferable createTransferable(JComponent argComponent) {
         VoList<T> list =  (VoList<T>) argComponent;
         ArrayList<T> objects = list.getMarkedItems();
-        System.out.print("num: " + objects.size() + " ");
-        System.out.println(objects.get(0).getClass().getCanonicalName());
+        LOGGER.info("num: " + objects.size() + " ");
+        LOGGER.info(objects.get(0).getClass().getCanonicalName());
         return new TransferableSO<T>(objects);
     } // createTransferable() method
         
+    @Override
+    public void exportDone(JComponent c, Transferable t, int action) {
+        super.exportDone(c, t, action);
+        LOGGER.info("DND: VoListTransferHandlerc.exportDone(): " + c.getBounds());
+    }
+    
 } // VoListTransferHandler class
