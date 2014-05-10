b3a97149b479ff6322c063fbcda520519f16b8e3
Dejan Lekic <dejan@lekic.org>
2014-05-10 15:42:34 +0000

    - Added header - Added a comment to the execute() method signature. Related issue(s): APC-978
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@791 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- src/main/java/com/areen/jlib/pattern/Command.java
+++ src/main/java/com/areen/jlib/pattern/Command.java
@@ -1,15 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Project: JLIB
+ * Version: $Id$ 
+ * License: (see below block, or the accompanied COPYING.txt file)
+ **************************************************************************************** LICENSE BEGIN *****
+ * Copyright (c) 2009-2014, JLIB AUTHORS (see the AUTHORS.txt file for the list of the individuals)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 
+ *  1) Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+ *     following disclaimer.
+ *  2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+ *     the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *  3) Neither the names of the organisations involved in the JLIB project, nor the names of their 
+ *     contributors to the JLIB project may be used to endorse or promote products derived from this software 
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************************** LICENSE END *****
+ * 
+ * This file is best viewed with 110 columns.
+12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+ * 
+ * In chronological order
+ * Author(s):
+ *   Dejan Lekic , http://dejan.lekic.org
+ * Contributor(s):
+ *   -
  */
 
 package com.areen.jlib.pattern;
 
 /**
- *
+ * An interface for the implementations of the Command pattern.
+ * More about this pattern: https://en.wikipedia.org/wiki/Command_pattern
+ * 
  * @author Dejan
  */
 public interface Command {
-    public void execute();
-}
+    /**
+     * Start executing the operation.
+     * Every implementation of this method will implement this method.
+     */
+    void execute();
+} // Command interface
+
+// $Id$
