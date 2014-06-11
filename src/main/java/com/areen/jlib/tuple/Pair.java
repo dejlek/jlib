e8a8958f365448928c69b69ec6693a962fd2fb8d
Dejan Lekic <dejan@lekic.org>
2014-06-11 17:45:33 +0000

    Added the BSD header to the Pair.java . Related issue(s): APC-2084
    git-svn-id: svn://srl009.adg.internal/jlib/trunk@804 bee1aa47-a06f-436c-a2f2-6a1efea3c100

--- src/main/java/com/areen/jlib/tuple/Pair.java
+++ src/main/java/com/areen/jlib/tuple/Pair.java
@@ -1,3 +1,42 @@
+/**
+ * Project: JLIB
+ * Version: $Id$ 
+ * License: (see below block, or the accompanied COPYING.txt file)
+ **************************************************************************************** LICENSE BEGIN *****
+ * Copyright (c) 2009-2014, JLIB AUTHORS (see the AUTHORS.txt file for the list of the individuals)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 
+ *  1) Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+ *     following disclaimer.
+ *  2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+ *     the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *  3) Neither the names of the organisations involved in the JLIB project, nor the names of their 
+ *     contributors to the JLIB project may be used to endorse or promote products derived from this software 
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************************** LICENSE END *****
+ * 
+ * This file is best viewed with 110 columns.
+12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+ * 
+ * In chronological order
+ * Author(s):
+ *   Dejan Lekic , http://dejan.lekic.org
+ * Contributor(s):
+ *   -
+ */
+
 package com.areen.jlib.tuple;
 
 import java.io.Serializable;
@@ -11,6 +50,8 @@ import java.util.Map;
  * of any Java Hash map...
  *
  * @author Dejan Lekic - http://dejan.lekic.org
+ * @param <KeyT> A type of the first element in the Pair.
+ * @param <ValueT> A type of the second element in the Pair.
  */
 public class Pair<KeyT, ValueT> implements Map.Entry<KeyT, ValueT>, Serializable {
 
@@ -48,7 +89,7 @@ public class Pair<KeyT, ValueT> implements Map.Entry<KeyT, ValueT>, Serializable
     @Override
     public String toString() {
         // previously we used " - " as a separator. Now we will use the 0x1f character, called the UNIT
-        // SEPARATOR to separate two fields in a String object. See the Sise class for more information.
+        // SEPARATOR to separate two fields in a String object. See the `Sise` class for more information.
         return first + "\u001f" + second;
     }
 
